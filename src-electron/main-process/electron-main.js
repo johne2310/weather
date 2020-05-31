c23616baf7d65c7b47ca0398548e76988fdbcace
johne2310 <john@day41.com.au>
2020-05-31 20:27:37 +1000

    v0.0.5

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,6 +1,26 @@
-import { app, BrowserWindow, nativeTheme } from 'electron';
+import {
+  app,
+  BrowserWindow,
+  nativeTheme,
+  Menu,
+  protocol,
+  ipcMain,
+} from 'electron';
 
+const log = require('electron-log');
 const { autoUpdater } = require('electron-updater');
+
+//-------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+//-------------------------------------------------------------------
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 try {
   if (
@@ -27,7 +47,7 @@ let mainWindow;
 
 function sendStatusToWindow(text) {
   log.info(text);
-  win.webContents.send('message', text);
+  mainWindow.webContents.send('message', text);
 }
 
 function createWindow() {
@@ -61,34 +81,6 @@ function createWindow() {
 
 // auto update start
 
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-});
-autoUpdater.on('update-available', info => {
-  sendStatusToWindow('Update available.');
-});
-autoUpdater.on('update-not-available', info => {
-  sendStatusToWindow('Update not available.');
-});
-autoUpdater.on('error', err => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-});
-autoUpdater.on('download-progress', progressObj => {
-  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message =
-    log_message +
-    ' (' +
-    progressObj.transferred +
-    '/' +
-    progressObj.total +
-    ')';
-  sendStatusToWindow(log_message);
-});
-autoUpdater.on('update-downloaded', info => {
-  sendStatusToWindow('Update downloaded');
-});
-
 //auto update end
 
 app.on('ready', createWindow);
@@ -105,6 +97,18 @@ app.on('activate', () => {
   }
 });
 
-app.on('ready', function() {
-  autoUpdater.checkForUpdatesAndNotify();
+ipcMain.on('app_version', event => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
 });
